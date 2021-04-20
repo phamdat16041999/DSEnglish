50ec8d8d54a7d0630bd36e39d49ae64af0a01759
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2021-04-20 13:12:03 +0700

    Update wsgi.py

--- DSEnglish/wsgi.py
+++ DSEnglish/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsenglish.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DSEnglish.settings')
 
 application = get_wsgi_application()
